955890e652a9bea85584897632cec4948b8ed97f
Adam Alenius <adam.alenius@gmail.com>
2021-12-02 17:56:02 +0100

    solve day2

---
+++ day2.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type PositionChange struct {
+	value     int
+	direction string
+}
+
+type Position struct {
+	x   int
+	y   int
+	aim int
+}
+
+func readDay2Input(lines []string) []PositionChange {
+	var positionChanges []PositionChange
+	for _, line := range lines {
+		splitLine := strings.Split(line, " ")
+		direction := splitLine[0]
+		rawValue := splitLine[1]
+		value, _ := strconv.Atoi(rawValue)
+		positionChanges = append(positionChanges, PositionChange{value, direction})
+	}
+
+	return positionChanges
+}
+
+func day2() {
+	wd, _ := os.Getwd()
+	lines := ReadNewlineSeparatedFile(wd + "/input/2.txt")
+	measurements := readDay2Input(lines)
+
+	log.Println(len(lines))
+
+	// pt 1
+	var position Position
+
+	for _, measurement := range measurements {
+		switch measurement.direction {
+		case "forward":
+			position.x = position.x + measurement.value
+		case "backward":
+			position.x = position.x - measurement.value
+		case "down":
+			position.y = position.y + measurement.value
+		case "up":
+			position.y = position.y - measurement.value
+		}
+	}
+	log.Println("pt1", position.x*position.y)
+
+	// pt 2
+	var position2 Position
+
+	for _, measurement := range measurements {
+		switch measurement.direction {
+		case "forward":
+			position2.x = position2.x + measurement.value
+			position2.y = position2.y + position2.aim*measurement.value
+		case "backward":
+			position2.x = position2.x - measurement.value
+		case "up":
+			position2.aim = position2.aim - measurement.value
+		case "down":
+			position2.aim = position2.aim + measurement.value
+		}
+
+		log.Println("between", position2)
+	}
+
+	log.Println("pos2", position2)
+	log.Println("pos2", position2.x*position2.y)
+
+}
